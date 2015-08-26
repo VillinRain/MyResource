d12f07aef86a89112752d04f77b9a658a3c1dbf6
VillinRain <384954842@qq.com>
2015-08-26 17:52:09 +0800

    单例

---
+++ src/parttern/singleton/SingletonDemo.java
@@ -0,0 +1,16 @@
+package parttern.singleton;
+
+public class SingletonDemo {
+	/**
+	 * 线程安全而且并发度高的单例变种
+	 * @author zhaochengyu
+	 *http://blog.sina.com.cn/s/blog_75247c770100yxpb.html#bsh-24-289558518
+	 */
+	private static class  SindletonDemoHandle{
+		public final static SingletonDemo instance=new SingletonDemo();
+	}
+	public static SingletonDemo getInstance(){
+		return SindletonDemoHandle.instance;
+		
+	}
+}
